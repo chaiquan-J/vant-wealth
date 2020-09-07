8ce120853aead2cfdb59024571d48d3c4ae8105e
chaiquan-J <1700159418@qq.com>
2020-09-07 19:30:08 +0800

    0.0.0
    第一次提交

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="login">
+		我是登录页
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return {
+				
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	
+</style>
